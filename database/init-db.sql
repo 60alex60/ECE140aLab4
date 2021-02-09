c31c940371bf767d3a233922e4d289f13147cba8
60alex60 <36143720+60alex60@users.noreply.github.com>
2021-02-08 23:32:06 -0800

    Add files via upload

--- database/init-db.sql
+++ database/init-db.sql
@@ -3,3 +3,8 @@ CREATE DATABASE IF NOT EXISTS lab4ece140a;
 USE lab4ece140a;
 
 -- Create Table here (Ensure it doesn't already exist!)
+ CREATE TABLE Commands (
+        id integer AUTO_INCREMENT PRIMARY KEY,
+        message VARCHAR(32) NOT NULL,
+        completed tinyint(1) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
